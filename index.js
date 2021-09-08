c7f8f43fb69bed0f11dc6685e1582bef95c9cada
giagia-git <79623168+giagia-git@users.noreply.github.com>
2021-09-08 03:24:04 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const app = express();
 const http = require("http").createServer(app);
-const mongoose = require("mongoose")
+//const mongoose = require("mongoose")
 const io = require("socket.io")(http, {
     CORS: {
         origin: '*',
@@ -15,9 +15,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
-mongoose.connect("mongodb+srv://giap92446:123456@cluster0.3cwko.mongodb.net/Database?retryWrites=true&w=majority",function() {
+/*mongoose.connect("mongodb+srv://giap92446:123456@cluster0.3cwko.mongodb.net/Database?retryWrites=true&w=majority",function() {
     console.log("Connect database success!");
-}); 
+}); */
 
 const PORT = process.env.PORT || 5000;
 
@@ -32,7 +32,7 @@ io.on("connection", (socket) => {
                 
     socket.emit("alluserconnection",{clientsConnection: clients});
 
-    socket.on("saveDatabase", (data) => {
+    /*socket.on("saveDatabase", (data) => {
         if(checkName.indexOf(data.username) <= -1) {
             checkName.push(data.username);
             const AccountUser = new AccountModel({ username: data.username });
@@ -44,7 +44,7 @@ io.on("connection", (socket) => {
                 }
             })
         }
-    })
+    })*/
 
     run();
     async function run() {
