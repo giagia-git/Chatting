016ac825c52f13ce0364d82808a2607b94a7f475
giagia-git <79623168+giagia-git@users.noreply.github.com>
2021-09-08 05:32:45 -0700

    Delete connectdb directory

--- connectdb/connectdb.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require("mongoose");
-
-module.exports = mongoose.connect("mongodb+srv://giap92446:123456@cluster0.3cwko.mongodb.net/Database?retryWrites=true&w=majority",function() {
-    console.log("Connect database success!");
-}); 
